4e3b89c38b5333781387f7da329f8d30fb62f2d8
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-09-16 11:44:58 +0300

    Add project files.

---
+++ AselsanCsharpEducation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AselsanCsharpEducation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberAsEmpty;
+            //value types
+            int number = 10;
+            byte numberAsByte = 200;
+            short numberAsShort = 100;
+            long numberAsLong = 100;
+            float numberFloat = 20.00f;
+            double numberDouble = 10.00;
+            decimal numberDecimal = 20.00m;
+            DateTime currentDate = DateTime.Now;
+
+            Console.WriteLine("number:" + number);
+
+            Console.WriteLine($"number:{number}");
+            //Console.WriteLine(`number:${number}`);
+            Console.WriteLine("number:{0} {1}", number, numberAsByte);
+
+            string rowProductName = "name:{0}";
+
+            Console.WriteLine(rowProductName, "ahmet");
+
+
+            //Console.WriteLine(numberAsEmpty);
+        }
+    }
+}
