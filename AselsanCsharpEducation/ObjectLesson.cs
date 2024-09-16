3dcbd933417661c7048cd0e428237f8dc8f5bfe8
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-09-16 14:09:23 +0300

    öğleden önce

---
+++ AselsanCsharpEducation/ObjectLesson.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AselsanCsharpEducation
+{
+    internal class Pen
+    {
+        // public
+        //internal
+        //Private
+        //field
+        internal string Color;
+        internal string Brand;
+        internal string Model;
+        internal decimal Price;
+        internal int Height;
+        internal int Width;
+        internal DateTime Created;
+
+
+        //Constructors
+
+        internal Pen()
+        {
+            Created = DateTime.Now;
+            Height = 20;
+            Width = 30;
+            Price = 100;
+        }
+
+
+        internal void Write()
+        {
+            Console.WriteLine($"Color:{Color}");
+            Console.WriteLine($"Brand:{Brand}");
+            Console.WriteLine($"Model:{Model}");
+            Console.WriteLine($"Price:{Price}");
+            Console.WriteLine($"Height:{Height}");
+            Console.WriteLine($"Width:{Width}");
+            Console.WriteLine($"Created:{Created}");
+        }
+
+        internal void WriteToConsole(string messageFromCache)
+        {
+            Console.WriteLine(messageFromCache);
+        }
+
+        internal string WriteToConsole()
+        {
+            return $"{Color}- {Brand}";
+        }
+
+        internal Tuple<string, string> ReturnTuple()
+        {
+            return new Tuple<string, string>(Color, Brand);
+        }
+
+        internal decimal CalculateKdv(decimal price, out int tax)
+        {
+            tax = 18;
+            return price * 1.18m;
+        }
+    }
+}
--- AselsanCsharpEducation/Program.cs
+++ AselsanCsharpEducation/Program.cs
@@ -6,32 +6,64 @@ namespace AselsanCsharpEducation
     {
         static void Main(string[] args)
         {
-            int numberAsEmpty;
-            //value types
-            int number = 10;
-            byte numberAsByte = 200;
-            short numberAsShort = 100;
-            long numberAsLong = 100;
-            float numberFloat = 20.00f;
-            double numberDouble = 10.00;
-            decimal numberDecimal = 20.00m;
-            DateTime currentDate = DateTime.Now;
+            Pen pen = new Pen
+            {
+                Brand = "Faber Castell",
+                Color = "Red",
+                Model = "123",
+            };
 
-            Console.WriteLine("number:" + number);
+            pen.Write();
+            //1.way
+            Console.WriteLine($"tuple 1.data :{pen.ReturnTuple().Item1}");
+            Console.WriteLine($"tuple 1.data :{pen.ReturnTuple().Item2}");
+            //2.way
+            var priceWithTax = pen.CalculateKdv(100, out int taxValue);
+            Console.WriteLine($"priceWithTax:{priceWithTax}");
+            Console.WriteLine($"taxvalue:{taxValue}");
 
-            Console.WriteLine($"number:{number}");
-            //Console.WriteLine(`number:${number}`);
-            Console.WriteLine("number:{0} {1}", number, numberAsByte);
+            Console.WriteLine("-------------------------");
+            Pen pen2 = new Pen
+            {
+                Brand = "Faber Castell",
+                Color = "Red",
+                Model = "123",
+                Price = 10.00m,
+                Height = 10,
+                Width = 5
+            };
+            pen2.Write();
 
-            string rowProductName = "name:{0}";
+            #region value types
 
-            Console.WriteLine(rowProductName, "ahmet");
+            //int numberAsEmpty;
+            ////value types
+            //int number = 10;
+            //byte numberAsByte = 200;
+            //short numberAsShort = 100;
+            //long numberAsLong = 100;
+            //float numberFloat = 20.00f;
+            //double numberDouble = 10.00;
+            //decimal numberDecimal = 20.00m;
+            //DateTime currentDate = DateTime.Now;
 
-            Console.WriteLine("numberAsByte:" + numberAsByte);
+            //Console.WriteLine("number:" + number);
 
-            Console.WriteLine("Merhaba Dünya");
+            //Console.WriteLine($"number:{number}");
+            ////Console.WriteLine(`number:${number}`);
+            //Console.WriteLine("number:{0} {1}", number, numberAsByte);
+
+            //string rowProductName = "name:{0}";
+
+            //Console.WriteLine(rowProductName, "ahmet");
+
+            //Console.WriteLine("numberAsByte:" + numberAsByte);
+
+            //Console.WriteLine("Merhaba Dünya");
 
             //Console.WriteLine(numberAsEmpty);
+
+            #endregion
         }
     }
 }
